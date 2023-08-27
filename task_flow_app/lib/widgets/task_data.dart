5198986695fb93b139169c903deb586c24a226f5
isaacscardoso <isaacscardosoblues@gmail.com>
2023-08-26 21:48:07 -0300

    Implementing the task exclusion confirmation modal.

--- task_flow_app/lib/widgets/task_data.dart
+++ task_flow_app/lib/widgets/task_data.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../bloc/exported_blocs.dart';
 import '../models/task_model.dart';
 import '../ui/styles/app_text_styles.dart';
 
@@ -13,7 +14,9 @@ final class TaskData extends StatelessWidget {
     return ListTile(
       trailing: Checkbox(
         value: task.isDone,
-        onChanged: (value) {},
+        onChanged: (_) {
+          context.read<TaskBloc>().add(TapCheckboxTaskEvent(task: task));
+        },
       ),
       title: Text(
         task.title,
@@ -25,6 +28,57 @@ final class TaskData extends StatelessWidget {
               style: context.appTextStyles.textRegular,
             )
           : null,
+      onLongPress: () {
+        showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog.adaptive(
+              backgroundColor: Colors.white,
+              title: RichText(
+                text: TextSpan(
+                  style: context.appTextStyles.textRegular.copyWith(
+                    color: Colors.black,
+                    fontSize: 18,
+                  ),
+                  children: <InlineSpan>[
+                    const TextSpan(text: 'Deseja excluir: '),
+                    TextSpan(
+                      text: task.title,
+                      style: context.appTextStyles.textExtraBold,
+                    ),
+                  ],
+                ),
+              ),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () {
+                    context.read<TaskBloc>().add(DeleteTaskEvent(task: task));
+                    Navigator.pop(context);
+                  },
+                  child: Text(
+                    'Sim',
+                    style: context.appTextStyles.textSemiBold
+                        .copyWith(color: Colors.red),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    backgroundColor: Colors.black,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: Text('Não', style: context.appTextStyles.textBold),
+                ),
+              ],
+            );
+          },
+        );
+      },
     );
   }
 }

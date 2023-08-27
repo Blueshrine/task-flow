5e883c1dce68dd13c009b201bf0c66a7566a74a5
isaacscardoso <isaacscardosoblues@gmail.com>
2023-08-27 16:30:31 -0300

    Changing the theme settings of the app.

--- task_flow_app/lib/ui/theme/theme_configuration.dart
+++ task_flow_app/lib/ui/theme/theme_configuration.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-
-import '../styles/app_colors.dart';
+//import '../styles/app_colors.dart';
 import '../styles/app_text_styles.dart';
 
 final class ThemeConfiguration {
-  ThemeConfiguration._();
+  const ThemeConfiguration._();
 
   // static final _defaultInputBorder = OutlineInputBorder(
   //   borderRadius: BorderRadius.circular(0),
@@ -15,14 +14,14 @@ final class ThemeConfiguration {
     useMaterial3: true,
     scaffoldBackgroundColor: Colors.white,
     appBarTheme: const AppBarTheme(
-      color: Colors.blue,
+      color: Colors.black,
       iconTheme: IconThemeData(color: Colors.white, size: 26),
     ),
-    colorScheme: ColorScheme.fromSeed(
-      seedColor: AppColors.instance.primary,
-      primary: AppColors.instance.primary,
-      secondary: AppColors.instance.secondary,
-    ),
+    // colorScheme: ColorScheme.fromSeed(
+    //   seedColor: AppColors.instance.primary,
+    //   primary: AppColors.instance.primary,
+    //   secondary: AppColors.instance.secondary,
+    // ),
     floatingActionButtonTheme: const FloatingActionButtonThemeData(
       foregroundColor: Colors.white,
       backgroundColor: Color.fromARGB(255, 9, 64, 136),

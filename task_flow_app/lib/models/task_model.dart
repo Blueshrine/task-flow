13ac9e6afeb55c264d20caeec75b89468074e9b7
isaacscardoso <isaacscardosoblues@gmail.com>
2023-08-27 02:46:27 -0300

    Adding the uuid generation library.

--- task_flow_app/lib/models/task_model.dart
+++ task_flow_app/lib/models/task_model.dart
@@ -1,23 +1,30 @@
-final class TaskModel {
+import 'package:equatable/equatable.dart';
+import 'package:uuid/uuid.dart';
+
+final class TaskModel extends Equatable {
+  final String uuid;
   final String title;
   final String? description;
   final bool? isDone;
   final bool? isDeleted;
 
-  const TaskModel({
+  TaskModel({
     required this.title,
+    String? uuid,
     this.description,
     this.isDone = false,
     this.isDeleted = false,
-  });
+  }) : uuid = uuid ?? const Uuid().v4();
 
   TaskModel copyWith({
+    String? uuid,
     String? title,
     String? description,
     bool? isDone,
     bool? isDeleted,
   }) {
     return TaskModel(
+      uuid: uuid ?? this.uuid,
       title: title ?? this.title,
       description: description ?? this.description,
       isDone: isDone ?? this.isDone,
@@ -27,6 +34,7 @@ final class TaskModel {
 
   Map<String, dynamic> toMap() {
     return {
+      'uuid': uuid,
       'title': title,
       'description': description,
       'isDone': isDone,
@@ -36,10 +44,14 @@ final class TaskModel {
 
   factory TaskModel.fromMap(Map<String, dynamic> map) {
     return TaskModel(
+      uuid: map['uuid'],
       title: map['title'] ?? '',
       description: map['description'],
       isDone: map['isDone'],
       isDeleted: map['isDeleted'],
     );
   }
+
+  @override
+  List<Object?> get props => [uuid, title, description, isDone, isDeleted];
 }
